1900a13d6a3d36b96410c7b365a132f953da46d6
Darwin <darwin.subramaniam@wdc.com>
2019-09-24 19:42:22 +0800

    added minio config to config service

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -4,58 +4,77 @@ import * as fs from 'fs';
 import * as Joi from '@hapi/joi';
 
 export interface EnvConfig {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 @Injectable()
 export class ConfigService {
-    private readonly envConfig: EnvConfig;
+  private readonly envConfig: EnvConfig;
 
-    constructor(filePath: string) {
-        const config = dotenv.parse(fs.readFileSync(filePath));
-        this.envConfig = this.validateInput(config);
+  constructor(filePath: string) {
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = this.validateInput(config);
+  }
 
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      APP_ENV: Joi.string()
+        .valid('development', 'production', 'test', 'provision')
+        .default('development'),
+      MONGO_SERVER_IP: Joi.string().default('localhost'),
+      MONGO_SERVER_PORT: Joi.number().default(27017),
+      MONGO_USER_NAME: Joi.string(),
+      MONGO_PASSWORD: Joi.string(),
+      APP_DATABASE_NAME: Joi.string(),
+      MINIO_SERVER_IP: Joi.string().default('localhost'),
+      MINIO_SERVER_PORT: Joi.number().default(9000),
+      MINIO_SERVER_SECRET: Joi.string(),
+      MINIO_SERVER_ACCESS_KEY: Joi.string(),
+    });
+
+    const { error, value: validatedEnvConfig } = envVarsSchema.validate(
+      envConfig,
+    );
+
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
     }
+    return validatedEnvConfig;
+  }
 
-    private validateInput(envConfig: EnvConfig): EnvConfig {
-        const envVarsSchema: Joi.ObjectSchema = Joi.object({
-            APP_ENV: Joi.string()
-                .valid('development', 'production', 'test', 'provision')
-                .default('development'),
-            MONGO_SERVER_IP: Joi.string().default('localhost'),
-            MONGO_SERVER_PORT: Joi.number().default(27017),
-            MONGO_USER_NAME: Joi.string(),
-            MONGO_PASSWORD: Joi.string(),
-            APP_DATABASE_NAME: Joi.string(),
-        });
+  get mongoServerIP(): string {
+    return String(this.envConfig.MONGO_SERVER_IP);
+  }
 
-        const { error, value: validatedEnvConfig } = envVarsSchema.validate(
-            envConfig,
-        );
+  get mongoServerPort(): number {
+    return Number(this.envConfig.MONGO_SERVER_PORT);
+  }
 
-        if (error) {
-            throw new Error(`Config validation error: ${error.message}`);
-        }
-        return validatedEnvConfig;
-    }
+  get mongoUserName(): string {
+    return String(this.envConfig.MONGO_USER_NAME);
+  }
 
-    get mongoServerIP() {
-        return String(this.envConfig.MONGO_SERVER_IP);
-    }
+  get mongoPassword(): string {
+    return String(this.envConfig.MONGO_PASSWORD);
+  }
 
-    get mongoServerPort() {
-        return Number(this.envConfig.MONGO_SERVER_PORT);
-    }
+  get appMainDBName(): string {
+    return String(this.envConfig.APP_DATABASE_NAME);
+  }
 
-    get mongoUserName() {
-        return String(this.envConfig.MONGO_USER_NAME);
-    }
+  get minioIP(): string {
+    return String(this.envConfig.MINIO_SERVER_IP);
+  }
 
-    get mongoPassword() {
-        return String(this.envConfig.MONGO_PASSWORD);
-    }
+  get minioPort(): number {
+    return Number(this.envConfig.MINIO_SERVER_PORT);
+  }
 
-    get appMainDBName() {
-        return String(this.envConfig.APP_DATABASE_NAME);
-    }
+  get minioSecret(): string {
+    return String(this.envConfig.MINIO_SERVER_SECRET);
+  }
+
+  get minioAccessKey(): string {
+    return String(this.envConfig.MINIO_SERVER_ACCESS_KEY);
+  }
 }
